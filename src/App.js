f34ec5e8366f83ddad76831651d4a5d60e12cc98
Sonia Kaul <soniakau@usc.edu>
2023-07-05 00:36:35 -0700

    made layout changes - ready to implement code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* header includes the spinning logo + title */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={require('./color-wheel.png')} className="App-logo" alt="logo" />
+        <p className="App-title">
+            ColorSnap
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="container">
+        {/* left div is for the upload section */}
+        <div className="App-upload">
+          <h1 className='section-title'> upload your image here </h1>
+        </div>
+        {/* right div is for the rendering section */}
+        <div className="App-render">
+          <h1 className='section-title'> color palette </h1>
+        </div>
+      </div>
     </div>
   );
 }
